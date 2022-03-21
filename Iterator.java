075ba0510c594a89214bfbb17a4693ddb6d7e380
Istvan Cseri <istvan.cseri@snowflake.com>
2022-03-20 17:25:50 -0700

    added locks

--- Iterator.java
+++ Iterator.java
@@ -1,9 +1,12 @@
+import java.util.Stack;
+import java.util.concurrent.locks.Lock;
 
 public class Iterator implements Enumerator {
 
     Node root;
     Node lastNode;
     Node nextNode;
+    Stack<Integer> path = new Stack<>();
 
     Iterator(Node root){
         this.root = root;
@@ -16,23 +19,33 @@ public class Iterator implements Enumerator {
             
     public Object nextElement() {
         Node next = nextNode;
+        Lock lock = next.takeRead();
+        System.out.println(String.format("Locking node %d", next.id));
+        lock.lock();
         if (nextNode.children.size() > 0) {
             nextNode = nextNode.children.get(0);
+            path.push(0);
             return next;
         }
+        else {
+            System.out.println(String.format("Un-locking node %d", next.id));
+            lock.unlock();
+        }
 
-        Node node = nextNode;
-        while(node.parent != null)
-        {
-            Node last = node;
-            node = node.parent;  
-            
-            for(int i = 0; i < node.children.size() - 1; i++)
-            {
-                if(last == node.children.get(i)){
-                    nextNode = node.children.get(i + 1);
-                    return next;
-                }   
+        while (!path.empty()) {
+            int ordinal = path.pop();
+            nextNode = nextNode.parent;
+            Lock lock1 = nextNode.takeRead();
+            System.out.println(String.format("Locking node %d", nextNode.id));
+            lock1.lock();
+            if (ordinal + 1 < nextNode.children.size()) {
+                path.push(ordinal + 1);
+                nextNode = nextNode.children.get(ordinal + 1);
+                return next;
+            }
+            else {
+                System.out.println(String.format("Un-locking node %d", nextNode.id));
+                lock1.unlock();
             }
         }
         nextNode = null;
@@ -44,7 +57,9 @@ public class Iterator implements Enumerator {
      * @param n the Node to delete
      */
     public void delete(Node n) {
+//        Lock lock = n.parent.takeWrite();
         n.parent.children.remove(n);
         n.parent = null;
+//        lock.unlock();
     }
 }
--- Node.java
+++ Node.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 class Node
 {
@@ -7,6 +10,7 @@ class Node
     List<Node> children;
     static int nextId = 0;
     int id;
+    ReadWriteLock lock;
   
     Node(Node parent){
         this.parent = parent;
@@ -15,10 +19,19 @@ class Node
         if (parent != null) {
             parent.children.add(this);
         }
+        lock = new ReentrantReadWriteLock();
     }
 
     @Override
     public String toString() {
         return String.format("Node: parent id = %d id = %d", parent == null ? -1 : parent.id, id);
+    }
+
+    Lock takeRead() {
+        return lock.readLock();
+    }
+
+    Lock takeWrite() {
+        return lock.writeLock();
     }
 }
